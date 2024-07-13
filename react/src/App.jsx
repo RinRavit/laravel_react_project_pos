96e61167a91a7b8eaffa4d80c8da214b6d4de5e4
Ravittt <pro@pros-MacBook-Pro.local>
2024-07-13 14:07:24 +0700

    clean code not use

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,83 +1,4 @@
-// code working on Category
-// import { createBrowserRouter, Navigate } from "react-router-dom";
-// import Dashboard from "./pages/admin/Dashboard.jsx";
-// import DefaultLayout from "./components/DefaultLayout.jsx";
-// import GuestLayout from "./components/GuestLayout.jsx";
-// import Login from "./views/Login.jsx";
-// import NotFound from "./views/NotFound.jsx";
-// import Users from "./views/Users.jsx";
-// import UserForm from "./views/UserForm.jsx";
-// import RoleStaff from "./components/RoleStaff.jsx";
-// import Category from "./pages/admin/Category.jsx";
 
-// const App = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <GuestLayout />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Navigate to="/login" />, // Redirect to login
-//       },
-//       {
-//         path: "login",
-//         element: <Login />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "/admin",
-//     element: <DefaultLayout />, // Admin routes
-//     children: [
-//       {
-//         path: "/admin",
-//         element: <Navigate to="/admin/dashboard" />, // Redirect to admin dashboard
-//       },
-//       {
-//         path: "dashboard",
-//         element: <Dashboard />,
-//       },
-//       {
-//         path: "category",
-//         element: <Category />,
-//       },
-//       {
-//         path: "users",
-//         element: <Users />,
-//       },
-//       {
-//         path: "users/new",
-//         element: <UserForm key="userCreate" />,
-//       },
-//       {
-//         path: "users/:id",
-//         element: <UserForm key="userUpdate" />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "/staff",
-//     element: <RoleStaff />, // Staff routes
-//     children: [
-//       {
-//         path: "/staff",
-//         element: <Navigate to="/staff/dashboard" />, // Redirect to staff dashboard
-//       },
-//       {
-//         path: "dashboard",
-//         element: <Dashboard />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "*",
-//     element: <NotFound />,
-//   },
-// ]);
-
-// export default App;
-
-// The end of code that working with Category
 
 // Code test product
 import { createBrowserRouter, Navigate } from "react-router-dom";
@@ -122,7 +43,7 @@ const App = createBrowserRouter([
       },
       {
         path: "dashboard",
-        element: <Dashboardstaff />,
+        element: <Dashboard />,
       },
       {
         path: "category",
--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,41 +1,4 @@
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useStateContext } from "../context/ContextProvider";
 
-// export default function GuestLayout() {
-//   const { user, token } = useStateContext();
-
-//   if (token) {
-//     return <Navigate to="/" />;
-//   }
-
-//   return (
-//     <div id="guestLayout">
-//       <Outlet />
-//     </div>
-//   );
-// }
-
-// // update 01
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useStateContext } from "../context/ContextProvider";
-
-// export default function GuestLayout() {
-//   const { token, user } = useStateContext();
-
-//   if (token) {
-//     return user.role === "admin" ? (
-//       <Navigate to="/admin" />
-//     ) : (
-//       <Navigate to="/staff" />
-//     );
-//   }
-
-//   return (
-//     <div id="guestLayout">
-//       <Outlet />
-//     </div>
-//   );
-// }
 
 // The Last Code update
 import { Navigate, Outlet } from "react-router-dom";
